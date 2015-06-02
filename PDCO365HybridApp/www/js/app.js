c6af36fae79413a7f89c36e72540b531b667e90d
juanmlarios <juan.larios@atlasconceptsolutions.com>
2015-06-02 12:30:39 -0400

    Fully functional Starting Point for Demo
    Demo is fully set up and functioning

--- PDCO365HybridApp/www/js/app.js
+++ PDCO365HybridApp/www/js/app.js
@@ -3,17 +3,94 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('app365', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+
+    // Layout page
+    .state('app', {
+        abstract: true,
+        url: "/app",
+        templateUrl: "layout/layout.html"
+    })
+
+    // Sign-in page
+     .state('sign-in', {
+         url: "/sign-in",
+         templateUrl: "auth/sign-in.html"
+     })
+
+    // Sign-out page
+        .state('app.sign-out', {
+            url: "/sign-out",
+            views: {
+                'mainContent': {
+                    templateUrl: "auth/sign-out.html"
+                }
+            }
+        })   
+
+    // Mail list page
+    .state('app.mail', {
+        url: "/mail",
+        views: {
+            'mainContent': {
+                templateUrl: "mail/mail-tabs.html"
+            }
+        }
+    })
+
+    // Mail list containing mails flagged as important
+    .state('app.mail.imp', {
+        url: "/imp/id:important",
+        views: {
+            "tab-imp-mail": {
+                templateUrl: "mail/mail-list.html"
+            }
+        }
+    })
+
+    // Mail detail page
+    .state('app.mail-detail', {
+        url: "/mail/:id",
+        views: {
+            'mainContent': {
+                templateUrl: "mail/mail-detail.html"
+            }
+        }
+    })
+
+    // Unread mail list page
+    .state('app.mail.unread', {
+        url: "/unread/id:unread",
+        views: {
+            "tab-unread-mail": {
+                templateUrl: "mail/mail-list.html"
+            }
+        }
+    })
+
+    // All mail list page
+    .state('app.mail.all', {
+        url: "/all/id:all",
+        views: {
+            "tab-all-mail": {
+                templateUrl: "mail/mail-list.html"
+            }
+        }
+    });
+
+    // Navigate to sign-in page when app starts.
+    $urlRouterProvider.otherwise('sign-in');
 })
